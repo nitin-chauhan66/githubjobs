063af16aa669787d236f86bb0b6dd3a6047155ca
nitinsfdc <nvch66@gmail.com>
2020-09-08 23:44:50 +0530

    Updates

---
+++ precache-manifest.2c355dfd95920ead18de096694e0e0cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ce24db4d41a045b09717413931d6b58",
+    "url": "/githubjobs/index.html"
+  },
+  {
+    "revision": "9061fd6c1f5b4b783a64",
+    "url": "/githubjobs/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "9061fd6c1f5b4b783a64",
+    "url": "/githubjobs/static/js/2.bb0094a8.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/githubjobs/static/js/2.bb0094a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7143eed0713c965711e",
+    "url": "/githubjobs/static/js/main.062b4fed.chunk.js"
+  },
+  {
+    "revision": "c6dd82e322a0a3ac7329",
+    "url": "/githubjobs/static/js/runtime-main.c3dc4207.js"
+  }
+]);
